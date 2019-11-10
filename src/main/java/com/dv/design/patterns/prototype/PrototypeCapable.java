b655ec1210228a717cd6c10c7322369ccb33d82e
Deepak Vijayan <deepak.vijayan@oracle.com>
2019-11-10 08:17:52 +0530

    removed proto package

--- src/main/java/com/dv/design/patterns/prototype/PrototypeCapable.java
+++
@@ -1,6 +0,0 @@
-package com.dv.design.patterns.prototype;
-
-public interface PrototypeCapable  extends Cloneable{
-
-    public PrototypeCapable clone() throws CloneNotSupportedException;
-}
--- src/main/java/com/dv/design/patterns/prototype/TestPrototypePattern.java
+++
@@ -1,7 +0,0 @@
-package com.dv.design.patterns.prototype;
-
-public class TestPrototypePattern {
-
-    public static void main(String[] args) {
-    }
-}
